74fdb2f75659a71639247970f0deab1503d17a47
Ben Winbinger <bmwinbinger@gmail.com>
2022-09-19 13:41:23 -0500

    Updated Main

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main{
   public static void main(String[] args){
-    System.out.println("hello world");
+    Backpack b1 = new Backpack("Medium" + 4 + "Tablet");
+    System.out.println(b1);
   }
 }
